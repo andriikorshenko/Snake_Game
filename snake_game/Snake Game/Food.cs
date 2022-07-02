2161d7e98c443c13c9943ae05b0a0c474c24fd72
Andrii Korshenko <andriikorshenko@gmail.com>
2022-07-02 16:38:07 +0300

    snake_game
    Code fixes

--- snake_game/Snake Game/Food.cs
+++ snake_game/Snake Game/Food.cs
@@ -14,8 +14,7 @@
                 food = new Pixel(random.Next(1, border.MapWidth - 3),
                     random.Next(1, border.MapHeight - 3),
                     ConsoleColor.Green);
-            } while (snake.Head.X == food.X && snake.Head.Y == food.Y 
-                     || snake.Body.Any(b => b.X == snake.Head.X && b.Y == snake.Head.Y));
+            } while (snake.Head.X == food.X && snake.Head.Y == food.Y || snake.Body.Any(b => b.X == snake.Head.X && b.Y == snake.Head.Y));
 
             return food; 
         }
